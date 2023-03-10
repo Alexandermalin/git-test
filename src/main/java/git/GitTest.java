82c58f519a0dae84a20ff6d5be30a1d621b655e4
xrx <xierongxin@yonyou.com>
2023-03-10 12:15:57 +0800

    测试提交

--- src/main/java/git/GitTest.java
+++ src/main/java/git/GitTest.java
@@ -9,6 +9,6 @@ public class GitTest {
         System.out.println("55555");
         System.out.println("6666");
         System.out.println("push test");
-        System.out.println("pull test);
+        System.out.println("pull test");
     }
 }
